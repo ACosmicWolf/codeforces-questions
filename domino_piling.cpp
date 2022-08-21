6b499d14eaa564c1d2ae7cc026f2400da4bbf980
Cosmic Wolf <95919584+ACosmicWolf@users.noreply.github.com>
2022-08-21 17:16:02 +0000

    Create domino_piling.cpp

---
+++ domino_piling.cpp
@@ -0,0 +1,16 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+     
+    int main() {
+        int l,b;
+        cin >> l >> b;
+        int b_area = l*b;
+        int d_area = 2;
+        int total = b_area/d_area;
+        
+     
+     
+        cout << total << endl;
+     
+        return 0;
+    }
